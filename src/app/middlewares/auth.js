7d01b7e29ba0af396cbda0accd7866629738edbb
alisson <psyalisson@gmail.com>
2019-08-28 20:22:21 -0300

    "teste"

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import authconfig from '../../config/auth';
 
+
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
