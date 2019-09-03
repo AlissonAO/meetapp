24cc31cafd02eb3d501bf93bc1da8b9819047398
alisson <psyalisson@gmail.com>
2019-09-02 22:17:35 -0300

    adicionando caso de uso file

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 import databaseconfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 class Database {
   constructor() {
     this.init();
@@ -12,6 +13,7 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseconfig);
     models.map((model) => model.init(this.connection));
+    models.map((model) => model.associate && model.associate(this.connection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,22 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer';
 import authMiddlerware from './app/middlewares/auth';
 
 import Usercontroller from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
+import FileController from './app/controllers/FileController';
 
 const routers = new Router();
-
+const upload = multer(multerConfig);
 
 routers.post('/users', Usercontroller.store);
 routers.post('/sessions', SessionController.store);
 
+routers.use(authMiddlerware);
 
-routers.put('/users', authMiddlerware, Usercontroller.update);
+routers.put('/users', Usercontroller.update);
+routers.post('/files', upload.single('file'), FileController.store);
+
+
 export default routers;
